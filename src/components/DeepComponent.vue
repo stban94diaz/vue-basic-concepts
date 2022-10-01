6a2508f803ca6cffc9996bcc7b7f0cded516d228
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2022-10-01 17:49:08 -0500

    :tada: Composition API - Template refs

--- src/components/DeepComponent.vue
+++ src/components/DeepComponent.vue
@@ -1,15 +1,19 @@
 <template>
   <div>User name: {{ userName }}</div>
+  <button ref="btn">Click me!!!</button>
 </template>
 
 <script>
-import { inject } from "vue";
+import { inject, ref, watch } from "vue";
 
 export default {
   setup() {
     const userName = inject("userName"); // by default this in not reactive for that use toRefs
+    const btn = ref(null);
 
-    return { userName };
+    watch(btn, () => console.log(btn));
+
+    return { userName, btn };
   },
 };
 </script>
