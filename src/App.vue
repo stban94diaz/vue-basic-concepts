d24314082b40c311b30ef8e50f3677a13baa181a
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2022-10-01 10:22:15 -0500

    :globe_with_meridians: Life cycle

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,30 @@ export default {
   data: () => ({
     show: false,
   }),
+  beforeCreate() {
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  created() {
+    console.log("created", this.$data, this.$el);
+  },
+  beforeMounted() {
+    console.log("beforeMounted", this.$data, this.$el);
+  },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate", this.$data, this.$el);
+  },
+  updated() {
+    console.log("updated", this.$data, this.$el);
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount", this.$data, this.$el);
+  },
+  unmounted() {
+    console.log("unmounted", this.$data, this.$el);
+  },
 };
 </script>
 
